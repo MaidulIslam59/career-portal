efa27a86fd7517de8f3be1dff51581068064b6af
MaidulIslam59 <maiduluapcse@gmail.com>
2024-10-06 18:09:53 +0600

    images path change

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="bg-[#7E90FE0D]">
+    <div className="bg-[#7E90FE0D] p-4">
       <div className="max-w-6xl mx-auto text-center">
         <div className="grid grid-cols-2 ">
           <div>
